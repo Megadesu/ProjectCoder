5e8c1339bab850b31966a47892ccf19d6c5f29f5
Megadesu <M.g.albornoz.94@gmail.com>
2023-05-10 20:56:45 -0300

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private ScoreManager m_scoreManager;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+
+        } else
+        {
+            Instance = this;    
+        }
+    }
+    public void AddScore(int p_score) { 
+    m_scoreManager.Add(p_score);
+    }
+}
