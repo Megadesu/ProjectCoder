0864cd501dc9715e967e62f6e11c0d72f7d0f1ad
EmanuelDavid <80975105+EmanuelDavid-Code01@users.noreply.github.com>
2023-05-17 21:38:11 -0300

    UIvicot2

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -9,16 +9,21 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private Enemy[] m_enemyToSpawn;
     [SerializeField] private List<Enemy> m_enemyList;
 
-    [ContextMenu("print name from last index")]
+    //[ContextMenu("print name from last index")]
 
-    private void Awake()
-    {
-        //m_enemyList = new List<Enemy>();
-        m_enemyToSpawn = new Enemy[5];
-       
-    }
-    private void GetNameFromLastIndex()
-    {
+    //private void Awake()
+    //{
+    //    m_enemyList.Add(m_enemysToSpawn[0]);
+    //    m_enemyList.RemoveAt(2);
+
+    //    var l_obtainedCostumer = TryGetCostumer(out Costumer p_myCostumer);
+    //    if (l_obtainedCostumer)
+    //    {
+    //       // Debug.Log(p_myCostumer.costumerName);
+    //    }
+    //}
+    //private bool TryGetCostumer(out Costumer p_costumer)
+    //{
         
-    }
-}
+    //}
+};
