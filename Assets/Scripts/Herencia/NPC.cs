f912900bbbf1869f8b5573199bf62ec0c563a8d7
EmanuelDavid <80975105+EmanuelDavid-Code01@users.noreply.github.com>
2023-06-05 16:34:07 -0300

    NPC comentado

--- Assets/Scripts/Herencia/NPC.cs
+++ Assets/Scripts/Herencia/NPC.cs
@@ -1,30 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public interface IInteractable
-{
-    bool IsInteractable { get; }
-    void Interact();
-}
-public class NPC : Entity, IInteractable
-{
-    [SerializeField] private string[] m_dialogue;
-    [SerializeField] private PlayableCharacter m_playableCharacter;
-    private void Awake()
-    {
-        GetName();
-        m_playableCharacter.GetHealth();
-    }
-    [ContextMenu("Try get health")]
+//public interface IInteractable
+//{
+//    bool IsInteractable { get; }
+//    void Interact();
+//}
+//public class NPC : Entity, IInteractable
+//{
+//    [SerializeField] private string[] m_dialogue;
+//    [SerializeField] private PlayableCharacter m_playableCharacter;
+//    private void Awake()
+//    {
+//        GetName();
+//        m_playableCharacter.GetHealth();
+//    }
+//    [ContextMenu("Try get health")]
     //private void TryGetHealth()
     //{if
     //    m_playableCharacter.GetHealth();
     //}
 
-    public bool IsInteractable { get;}
-    public void Interact()
-    {
+//    public bool IsInteractable { get;}
+//    public void Interact()
+//    {
 
-    }
-}
+//    }
+//}
